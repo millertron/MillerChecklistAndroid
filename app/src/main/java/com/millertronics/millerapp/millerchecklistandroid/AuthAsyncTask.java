702bb3ee73239c331e8762691e2cdc5fd4b83cef
millertronv <senjinkoha@gmail.com>
2017-06-29 09:50:56 +0100

    Move AsyncTask to external class

---
+++ app/src/main/java/com/millertronics/millerapp/millerchecklistandroid/AuthAsyncTask.java
@@ -0,0 +1,128 @@
+package com.millertronics.millerapp.millerchecklistandroid;
+
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
+import android.os.AsyncTask;
+import android.support.v7.app.AlertDialog;
+import android.util.Log;
+
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+
+import javax.net.ssl.HttpsURLConnection;
+
+/**
+ * Created by koha.choji on 29/06/2017.
+ */
+
+public class AuthAsyncTask extends AsyncTask<String, Void, String> {
+
+    private MainActivity mainActivity;
+    private static final String DELIMITER = "%BREAK%";
+    private ProgressDialog progressDialog;
+
+    public AuthAsyncTask(MainActivity mainActivity){
+        this.mainActivity = mainActivity;
+        progressDialog = new ProgressDialog(this.mainActivity);
+    }
+
+    @Override
+    protected void onPreExecute(){
+        progressDialog.setMessage(mainActivity.getString(R.string.submitting_request));
+        progressDialog.show();
+    }
+
+    @Override
+    protected String doInBackground(String... params) {
+        String authUrlString = mainActivity.getString(R.string.default_mutex_url)
+                + mainActivity.getString(R.string.api_prefix)
+                + mainActivity.getString(R.string.api_authentication_path);
+        try {
+            URL authUrl = new URL(authUrlString);
+            HttpsURLConnection connection = (HttpsURLConnection) authUrl.openConnection();
+            connection.setReadTimeout(60 * 1000);
+            connection.setConnectTimeout(60 * 1000);
+            connection.setRequestMethod("POST");
+            connection.setRequestProperty(mainActivity.getString(R.string.api_content_type),
+                    mainActivity.getString(R.string.api_content_type_json));
+            connection.setRequestProperty(mainActivity.getString(R.string.api_auth_id),
+                    params[0]);
+            connection.setRequestProperty(mainActivity.getString(R.string.api_auth_key),
+                    params[1]);
+            int responseCode = connection.getResponseCode();
+
+            BufferedReader in = new BufferedReader(
+                    new InputStreamReader( responseCode == 200 ? connection.getInputStream()
+                            : connection.getErrorStream()));
+            String inputLine;
+            StringBuffer response = new StringBuffer();
+
+            response.append(responseCode);
+            response.append(DELIMITER);
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            }
+            in.close();
+
+            return response.toString();
+
+        } catch (Exception e){
+            Log.e(AuthAsyncTask.class.getName(), Log.getStackTraceString(e));
+        }
+        return "Error in running async task!";
+    }
+
+    @Override
+    protected void onPostExecute(String result){
+        super.onPostExecute(result);
+        if (progressDialog.isShowing()){
+            progressDialog.dismiss();
+        }
+        final String[] responseParams = result.split(DELIMITER);
+
+        String message = null;
+        final int statusCode = Integer.parseInt(responseParams[0]);
+        if (statusCode == 200){
+            try {
+                JSONObject jsonObj = new JSONObject(responseParams[1]);
+                StringBuilder sb = new StringBuilder();
+                sb.append("Welcome ")
+                        .append(jsonObj.getString("first_name"))
+                        .append(" ")
+                        .append(jsonObj.getString("last_name"))
+                        .append("!");
+                message = sb.toString();
+
+                User user = new User(jsonObj.getInt("id"),
+                        jsonObj.getString("username"),
+                        jsonObj.getString("first_name"),
+                        jsonObj.getString("last_name"),
+                        jsonObj.getString("email"),
+                        jsonObj.getString("api_key"));
+
+                User.setCurrentUser(user);
+
+                mainActivity.displayDashboard();
+            } catch (Exception e){
+                Log.e(mainActivity.getClass().getName(), Log.getStackTraceString(e));
+                message = responseParams[1];
+            }
+        }else {
+            message = responseParams[1];
+            AlertDialog.Builder builder = new AlertDialog.Builder(mainActivity);
+            builder.setMessage(message);
+            builder.setCancelable(false);
+            builder.setPositiveButton(R.string.dialog_ok,
+                    new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface di, int i) {
+                            mainActivity.enableLoginButton();
+                            di.cancel();
+                        }
+                    });
+            builder.show();
+        }
+    }
+}
--- app/src/main/java/com/millertronics/millerapp/millerchecklistandroid/MainActivity.java
+++ app/src/main/java/com/millertronics/millerapp/millerchecklistandroid/MainActivity.java
@@ -56,7 +56,7 @@ public class MainActivity extends AppCompatActivity {
         } else return false;
     }
 
-    private void displayDashboard() {
+    public void displayDashboard() {
         if (viewFlipper.getDisplayedChild() != 0) {
             viewFlipper.setDisplayedChild(0);
         }
@@ -116,115 +116,6 @@ public class MainActivity extends AppCompatActivity {
     private void submitAndProcessAuthenticationRequest() throws IOException {
         AuthAsyncTask authAsyncTask = new AuthAsyncTask(this);
         authAsyncTask.execute(usernameInput.getText().toString(), passwordInput.getText().toString());
-    }
-
-    private class AuthAsyncTask extends AsyncTask<String, Void, String> {
-
-        private MainActivity mainActivity;
-        private static final String DELIMITER = "%BREAK%";
-        private ProgressDialog progressDialog;
-
-        public AuthAsyncTask(MainActivity mainActivity){
-            this.mainActivity = mainActivity;
-            progressDialog = new ProgressDialog(this.mainActivity);
-        }
-
-        @Override
-        protected void onPreExecute(){
-            progressDialog.setMessage(mainActivity.getString(R.string.submitting_request));
-            progressDialog.show();
-        }
-
-        @Override
-        protected String doInBackground(String... params) {
-            String authUrlString = getString(R.string.default_mutex_url)
-                    + getString(R.string.api_prefix)
-                    + getString(R.string.api_authentication_path);
-            try {
-                URL authUrl = new URL(authUrlString);
-                HttpsURLConnection connection = (HttpsURLConnection) authUrl.openConnection();
-                connection.setReadTimeout(60 * 1000);
-                connection.setConnectTimeout(60 * 1000);
-                connection.setRequestMethod("POST");
-                connection.setRequestProperty(getString(R.string.api_content_type),
-                        getString(R.string.api_content_type_json));
-                connection.setRequestProperty(getString(R.string.api_auth_id),
-                        params[0]);
-                connection.setRequestProperty(getString(R.string.api_auth_key),
-                        params[1]);
-                int responseCode = connection.getResponseCode();
-
-                BufferedReader in = new BufferedReader(
-                        new InputStreamReader( responseCode == 200 ? connection.getInputStream()
-                        : connection.getErrorStream()));
-                String inputLine;
-                StringBuffer response = new StringBuffer();
-
-                response.append(responseCode);
-                response.append(DELIMITER);
-                while ((inputLine = in.readLine()) != null) {
-                    response.append(inputLine);
-                }
-                in.close();
-
-                return response.toString();
-
-            } catch (Exception e){
-                Log.e(AuthAsyncTask.class.getName(), Log.getStackTraceString(e));
-            }
-            return "Error in running async task!";
-        }
-
-        @Override
-        protected void onPostExecute(String result){
-            super.onPostExecute(result);
-            if (progressDialog.isShowing()){
-                progressDialog.dismiss();
-            }
-            final String[] responseParams = result.split(DELIMITER);
-
-            String message = null;
-            final int statusCode = Integer.parseInt(responseParams[0]);
-            if (statusCode == 200){
-                try {
-                    JSONObject jsonObj = new JSONObject(responseParams[1]);
-                    StringBuilder sb = new StringBuilder();
-                    sb.append("Welcome ")
-                            .append(jsonObj.getString("first_name"))
-                            .append(" ")
-                            .append(jsonObj.getString("last_name"))
-                            .append("!");
-                    message = sb.toString();
-
-                    User user = new User(jsonObj.getInt("id"),
-                            jsonObj.getString("username"),
-                            jsonObj.getString("first_name"),
-                            jsonObj.getString("last_name"),
-                            jsonObj.getString("email"),
-                            jsonObj.getString("api_key"));
-
-                    User.setCurrentUser(user);
-
-                    mainActivity.displayDashboard();
-                } catch (Exception e){
-                    Log.e(mainActivity.getClass().getName(), Log.getStackTraceString(e));
-                    message = responseParams[1];
-                }
-            }else {
-                message = responseParams[1];
-                AlertDialog.Builder builder = new AlertDialog.Builder(mainActivity);
-                builder.setMessage(message);
-                builder.setCancelable(false);
-                builder.setPositiveButton(R.string.dialog_ok,
-                        new DialogInterface.OnClickListener() {
-                            public void onClick(DialogInterface di, int i) {
-                                mainActivity.enableLoginButton();
-                                di.cancel();
-                            }
-                        });
-                builder.show();
-            }
-        }
     }
 
     public void enableLoginButton(){
